21b82b4f3793b31e0d7a47ae326f08a35553e1c0
HANITZ <111544000+HANITZ@users.noreply.github.com>
2023-04-05 21:13:04 +0900

    [level 2] Title: 문자열 압축, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -1,60 +1,33 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-const solution = (s) => {
-        let result = s.length; // 문자열의 최대개수.
-        for (let i = 1; i <= Math.floor(s.length / 2); i++) {
-          // 최대 절반까지비교.
-          let temp = ""; // 문자열 임시저장.
-          let cnt = 1; // 같은 문자열 count.
+function solution(s) {
+    let st,ed,prior,left,cnt,tmp
+    var answer = s.length
+    const half = Math.floor(s.length/2)
+    for(let l=half; l>0; l--){
+        tmp=''
+        for(let i=0; i<s.length; i+=l){
 
-          for (let j = 0; j < s.length; j += i) { // j += i, i = 1... n/2개씩 증가.
-            let s1 = s.slice(j, j + i); // 비교대상 문자열
-
-            while (true) { // 같은 문자열 찾는 루프.
-              j += i; // 그 다음 문자열 index
-              let s2 = s.slice(j, j + i);
-              if (s1 === s2) {
-                // 비교문자열과 같은 문자열이라면 cnt증가.
-                cnt++;
-              } else {
-                // 다른 문자열이라면 break;
-                break;
-              }
+            prior = s.slice(i, i+l)
+            cnt=1
+            i+=l
+            left = s.slice(i,i+l)
+            while (prior === left){
+                i+=l
+                left = s.slice(i, i+l)
+                cnt++
             }
+            
+            if(cnt>1){
+                tmp += cnt+prior
+            }else{
+                tmp += prior
+            }
+            i-=l
 
-            temp += cnt >= 2 ? cnt + s1 : s1; // 2번이상 반복된다면 압축.
-            j -= i; // 다음 for문에서 증가되므로 한번 감소.
-            cnt = 1; // cnt를 초기화.
-          }
-          result = Math.min(temp.length, result); // 단순 문자열길이 이므로 최소값만 저장.
         }
-        return result;
-      };
+        answer = Math.min(answer, tmp.length)
+        
+    }
+
+
+    return answer;
+}
