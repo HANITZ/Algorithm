0249a54287012a4d4a772f401e1ce138bba7fc37
HANITZ <111544000+HANITZ@users.noreply.github.com>
2023-05-03 01:35:02 +0900

    Time: 1059 ms (5.2%), Space: 18.7 MB (5.62%) - LeetHub

--- LeetCode/Medium/0743-network-delay-time/0743-network-delay-time.py
+++ LeetCode/Medium/0743-network-delay-time/0743-network-delay-time.py
@@ -1,19 +1,29 @@
 from heapq import heappush,heappop
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = collections.defaultdict(list)
-        for u, v, w in times:
-            graph[u].append((v,w))
 
-        Q = [(0,k)]
-        dist = collections.defaultdict(int)
-        while Q:
-            time, node = heappop(Q)
-            if node not in dist:
-                dist[node] = time
-                for v, w in graph[node]:
-                    heappush(Q,(time+w, v))
+        graph = [[-1 for _ in range(n+1)] for _ in range(n+1)]
+        for st, ed, weight in times:
+            graph[st][ed] = weight
+        roots = [21e8]*(n+1)
+        roots[0] = 0
+        roots[k] = 0
+        heap = [(0, k)]
+        while heap:
+            time, st =heappop(heap)
+            roots[st] = min(roots[st], time)
+
+            for ed in range(1, n+1):
+                if graph[st][ed] != -1:
+                    if roots[ed] > graph[st][ed]+time:
+                        print(graph[st][ed], time, st, ed)
+                        heappush(heap, (graph[st][ed]+ time, ed))
+        print(roots)
+        if 21e8 in roots:
+            return -1
+        else:
+            return max(roots)
+
+
         
-        if len(dist) == n:
-            return max(dist.values())
-        return -1
+        
