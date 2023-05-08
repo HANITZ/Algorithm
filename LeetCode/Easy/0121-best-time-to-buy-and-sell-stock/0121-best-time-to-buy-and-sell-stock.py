024a0f9ce0eba090ad01a989d2f140e0211f9fae
HANITZ <111544000+HANITZ@users.noreply.github.com>
2023-05-08 14:52:56 +0900

    Time: 981 ms (77.96%), Space: 27.4 MB (10.27%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
+        answer = 0
         _min = 10**4
         for price in prices:
             if _min > price:
                 _min = price
             else:
-                profit = max(profit, price - _min)
-        return profit
+                answer = max(answer, price - _min)
+        return answer
         
